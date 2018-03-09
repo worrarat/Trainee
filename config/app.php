2f3938fc1f463132ac8b4d78a7a74f991bd63ae9
Worrarat.j <navigatae@gmail.com>
2018-03-09 16:18:23 +0700

    Adminlte test
    test

--- config/app.php
+++ config/app.php
@@ -146,6 +146,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+		Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class,
+		App\Providers\RouteServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -208,6 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+		'AdminLTE' => Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class,
 
     ],
 
